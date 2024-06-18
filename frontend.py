05449a75dff7e441881d80e79adb39b5c10fd13a
tadeo <94108039+tadeongmi@users.noreply.github.com>
2024-06-18 17:16:42 +0200

    add figma embed

--- frontend.py
+++ frontend.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import streamlit.components.v1 as components
 from argentinian_tvl import main
 
 st.set_page_config(
@@ -16,13 +17,20 @@ st.set_page_config(
     }
 )
 
-
-st.title('Argentinean Crypto Projects Analytics')
-
+# load dataframe
 df = main()
-# st.dataframe(df,use_container_width=True)
 
+# filter for 2023 onwards
 df = df[df['date'] > '2023-01-01']
 
+# title
+st.title('Argentinean Crypto Ecosystem')
+
+#tvl chart
+st.header('Argentinean Crypto Projects by Total Value Locked')
 fig = px.area(df, x="date", y="tvl", color="protocol")
 st.plotly_chart(fig, use_container_width=True)
+
+# ecosystem landscape (figma embed)
+figma_url = "https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fdesign%2FPUo88BOv4TnB6g5wvE7u3H%2FCrypto-Landscape-in-Argentina%3Fnode-id%3D0-1%26t%3DmtgupHxoF481CKB4-1"
+components.iframe(figma_url, width=None, height=800, scrolling=False)
