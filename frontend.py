8ec43d81cf59ad3d5384c9725260d9d3d19d8d23
rodriguezmatid <matias.demian.r@gmail.com>
2024-07-21 19:12:53 -0300

    frontend

--- frontend.py
+++ frontend.py
@@ -4,6 +4,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 from argentinian_tvl import main
 import protocols.poap as poap
+import protocols.decentraland as decentraland
+import protocols.exactly as exactly
 
 st.set_page_config(
     page_title="Crecimiento Research",
@@ -113,3 +115,92 @@ with colc:
     hist_collec = pd.DataFrame(poap.poap_historic_collections())
     fig_c = px.line(hist_collec, x="time", y="amount")
     st.plotly_chart(fig_c, use_container_width=True)
+
+# Decentraland
+st.header('DECENTRALAND')
+cold, cole = st.columns(2)
+
+with cold:
+    st.metric(label='Holders',value=pretty_number(decentraland.main_stats()[0]))
+
+with cole:
+    st.metric(label='Collection Assets',value=pretty_number(decentraland.main_stats()[1]))
+
+colf, colg, colh, coli = st.columns(4)
+with colf:
+    st.metric(label='7 Days Volume',value=pretty_number(decentraland.timeline_stats()[0]*1e3))
+
+with colg:
+    st.metric(label='All Time Volume',value=pretty_number(decentraland.timeline_stats()[1]*1e3))
+
+with colh:
+    st.metric(label='7 Days Sales',value=pretty_number(decentraland.timeline_stats()[2]))
+
+with coli:
+    st.metric(label='All Time Sales',value=pretty_number(decentraland.timeline_stats()[3]*1e3))
+
+st.write('Historic trends')
+historic_values = pd.DataFrame(decentraland.historic_floor_and_volumes())
+colj, colk = st.columns(2)
+
+with colj:
+    df_volume = historic_values[['time', 'volume']]
+    fig_a = px.line(df_volume, x="time", y="volume", title="Volume")
+    st.plotly_chart(fig_a, use_container_width=True)
+
+with colk:
+    df_buyers = historic_values[['time', 'buyers']]
+    fig_b = px.line(df_buyers, x="time", y="buyers", title="Buyers")
+    st.plotly_chart(fig_b, use_container_width=True)
+
+coll, colm = st.columns(2)
+
+with coll:
+    df_floor = historic_values[['time', 'floor']]
+    fig_c = px.line(df_floor, x="time", y="floor", title="USD Floor Price")
+    st.plotly_chart(fig_c, use_container_width=True)
+
+with colm:
+    df_floor_eth = historic_values[['time', 'floor_eth']]
+    fig_d = px.line(df_floor_eth, x="time", y="floor_eth", title="Eth Floor Price")
+    st.plotly_chart(fig_d, use_container_width=True)
+
+# Exactly Finance
+st.header('EXACTLY')
+coln, colo, colp = st.columns(3)
+
+with coln:
+    st.metric(label='Total Tx',value=pretty_number(exactly.total_transactions()*1))
+
+with colo:
+    st.metric(label='Active Depositors',value=pretty_number(exactly.active_depositors()/1))
+
+with colp:
+    st.metric(label='Active Borrowers',value=pretty_number(exactly.active_borrowers()/1))
+
+colq, colr = st.columns(2)
+with colq:  
+        st.write('Daily active users')
+        daily_active_users = pd.DataFrame(exactly.daily_active_users())
+        fig_b = px.line(daily_active_users, x="day", y="total")
+        st.plotly_chart(fig_b, use_container_width=True)
+
+with colr:     
+        st.write('Total users')
+        total_users = pd.DataFrame(exactly.total_users())
+        # st.write("Columns in the dataframe: ", total_users.columns.tolist())
+        fig_b = px.line(total_users, x="day", y="users")
+        st.plotly_chart(fig_b, use_container_width=True)
+
+cols, colt = st.columns(2)
+with cols:  
+        st.write('Historic depositors')
+        historic_depositors = pd.DataFrame(exactly.historic_depositors())
+        fig_b = px.line(historic_depositors, x="day", y="users")
+        st.plotly_chart(fig_b, use_container_width=True)
+
+with colt:  
+        st.write('Historic borrowers')
+        historic_borrowers = pd.DataFrame(exactly.historic_borrowers())
+        fig_b = px.line(historic_borrowers, x="day", y="users")
+        st.plotly_chart(fig_b, use_container_width=True)
